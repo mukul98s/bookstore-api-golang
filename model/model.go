e829f81fc0ecdf2fba118837164115eaa7e5d75c
Mukul Sharma <mymukul112@gmail.com>
2023-04-10 23:02:11 +0530

    add password and update time to user model

--- model/model.go
+++ model/model.go
@@ -7,7 +7,9 @@ type User struct {
 	Name      string    `json:"name" validate:"required,min=3"`
 	Email     string    `json:"email" validate:"required,email"`
 	Phone     string    `json:"phone" validate:"required,min=10,max=10"`
+	Password  string    `json:"password" validate:"required,min=8"`
 	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type Book struct {
